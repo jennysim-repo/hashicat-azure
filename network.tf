7cbe0cd18a8e7d66500795364ea0ca6df88419b1
jaeheesim <jaehee1440@gmail.com>
2023-12-22 10:00:49 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -1,6 +1,6 @@
 module "network" {
   source  = "app.terraform.io/hashicorp-chip/network/azurerm"
   version = "3.5.0"
-  resource_group_name = "dodari"
+  resource_group_name = "myresourcegroup"
   # insert required variables here
 }
