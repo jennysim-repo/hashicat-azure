a6ce7dc07499d25ae04628793c780d8c856ebc68
jaeheesim <jaehee1440@gmail.com>
2023-12-22 10:03:08 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -1,6 +1,6 @@
 module "network" {
   source  = "app.terraform.io/hashicorp-chip/network/azurerm"
   version = "3.5.0"
-  resource_group_name = "myresourcegroup"
+  resource_group_name = "${var.prefix}-workshop"
   # insert required variables here
 }
